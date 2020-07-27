55bf9e0d41a9bc7e34451b4df4ea51747700f530
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-07-26 20:22:36 -0700

    extract nested zips utility script

---
+++ extract_zips.py
@@ -0,0 +1,29 @@
+from io import BytesIO
+import zipfile
+from pathlib import Path
+
+
+cwd = Path(__file__).parent.absolute()
+data_dir = cwd / 'data'
+partial_dir = data_dir / 'partial'
+
+if not partial_dir.exists():
+    partial_dir.mkdir(exist_ok=True)
+
+try:
+    ziped_dm_data = list(data_dir.glob('*.zip'))[0]
+except:
+    raise Exception("Is there a zip file in the data dir?")
+
+with zipfile.ZipFile(ziped_dm_data) as zip_ref:
+    unzip_count = 5
+
+    for spl_zip in zip_ref.namelist():
+        if not unzip_count:
+            break
+        unzip_count -= 1
+        nested_zip_data = BytesIO(zip_ref.read(spl_zip))
+        with zipfile.ZipFile(nested_zip_data) as nested_zip:
+            for unzip_file in nested_zip.namelist():
+                if unzip_file.endswith('xml'):
+                    nested_zip.extract(unzip_file, partial_dir)
