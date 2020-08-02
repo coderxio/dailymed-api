9383091210160a28b7cfb48c1d29c7715c3cfbdb
Caleb Dunn <finish.06@gmail.com>
2020-08-02 14:35:53 -0500

    single line with & finally to close

--- get_zips.py
+++ get_zips.py
@@ -9,9 +9,10 @@ if not data_dir.exists():
     data_dir.mkdir(exist_ok=True)
 
 try:
-    for i in range(1, 5):
-        with request.urlopen(f'ftp://public.nlm.nih.gov/nlmdata/.dailymed/dm_spl_release_human_rx_part{i}.zip') as r:
-            with open(f'{data_dir}/spl_part{i}.zip', 'wb') as f:
+    for i in range(1, 2):
+        with request.urlopen(f'ftp://public.nlm.nih.gov/nlmdata/.dailymed/dm_spl_release_human_rx_part{i}.zip') as r, open(f'{data_dir}/spl_part{i}.zip', 'wb') as f:
                 f.write(r.read())
 except Exception as err:
     raise (f"Unable to perform request: {err}")
+finally:
+    print("Downloads complete")
