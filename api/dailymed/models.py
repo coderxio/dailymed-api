88c5c09e09b375064cbdf4f3a8c0e9a63e683686
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-27 20:24:20 -0700

    Basic models

--- api/dailymed/models.py
+++ api/dailymed/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Set(models.Model):
+    """Dailymed set model which can contain multiple spl files"""
+    name = models.CharField(100)
+
+
+class Spl(models.Model):
+    """Dailymed model for individual spl xml files"""
+    name = models.CharField(100)
+    set = models.ForeignKey(
+        Set,
+        on_delete=models.PROTECT,
+        related_name="spls"
+    )
+
+class Ndc(models.Model):
+    value = models.CharField(15)
+    spl = models.ForeignKey(
+        Spl,
+        on_delete=models.PROTECT,
+        related_name="ndcs"
+    )
