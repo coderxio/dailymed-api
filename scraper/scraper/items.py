b159273c94152978112cb31a505fdcd4f2d63be5
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-28 07:04:34 -0700

    Basic scrapy items

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -6,7 +6,13 @@
 import scrapy
 
 
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class SetItem(scrapy.Item):
+    name = scrapy.Field()
+    spls = scrapy.Field()
+
+class SplItem(scrapy.Item):
+    name = scrapy.Field()
+    ndcs = scrapy.Field()
+
+class NdcItem(scrapy.Item):
+    value = scrapy.Field()
