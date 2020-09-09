31d4db72efc0d1d171552a39f76ba7519d5c97be
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-09-09 08:38:44 -0700

    Set robots.txt setting to false

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -31,7 +31,7 @@ NEWSPIDER_MODULE = 'scraper.spiders'
 #USER_AGENT = 'scraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
