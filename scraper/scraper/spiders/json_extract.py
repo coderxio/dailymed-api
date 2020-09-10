f64bee7f3e3a48d330fac3d5c65bb0985f57a85a
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-09-09 17:50:49 -0700

    Ignore active and inactive ing for now

--- scraper/scraper/spiders/json_extract.py
+++ scraper/scraper/spiders/json_extract.py
@@ -2,7 +2,6 @@ import scrapy
 from scrapy.loader import ItemLoader
 from scraper.utils import get_filenames
 from scraper.items import SplItem, ProductItem, PackageItem
-from dailymed.models import Set, Spl, Ndc
 
 
 class JsonSpider(scrapy.Spider):
@@ -23,8 +22,8 @@ class JsonSpider(scrapy.Spider):
             product_il = ItemLoader(item=ProductItem(), selector=product)
             product_il.add_xpath('code', './manufacturedProduct/code/@code')
             product_il.add_xpath('name', './manufacturedProduct/name/text()')
-            product_il.add_xpath('active_ing', './/ingredient[starts-with(@classCode, "ACT")]/ingredientSubstance/name/text()')
-            product_il.add_xpath('inactive_ing', './/ingredient[starts-with(@classCode, "IACT")]/ingredientSubstance/name/text()')
+            #  product_il.add_xpath('active_ing', './/ingredient[starts-with(@classCode, "ACT")]/ingredientSubstance/name/text()')
+            #  product_il.add_xpath('inactive_ing', './/ingredient[starts-with(@classCode, "IACT")]/ingredientSubstance/name/text()')
 
             for package in product.xpath('.//containerPackagedProduct'):
                 package_il = ItemLoader(item=PackageItem(), selector=package)
