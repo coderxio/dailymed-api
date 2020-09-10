1480f896500b4b100fab787b0f99cded500211d6
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-09-09 17:50:19 -0700

    Enable pipeline

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -76,9 +76,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scraper.pipelines.ScraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+  'scraper.pipelines.ScraperPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
