0ce8c6c1c4a65819732057512d763b0fb44323bd
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-09-14 18:42:48 -0700

    Adjust pipeline to load inactive ingredients into db

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -5,7 +5,7 @@
 
 
 # useful for handling different item types with a single interface
-from dailymed.models import Set
+from dailymed.models import Set, InactiveIngredient
 
 
 class ScraperPipeline:
@@ -17,7 +17,29 @@ class ScraperPipeline:
 
         for product_data in products_data:
             packages_data = product_data.pop('packages')
+            inactive_ingredients_list = []
+
+            if 'inactive_ingredients' in product_data:
+                inactive_ingredients_data = product_data.pop(
+                    'inactive_ingredients'
+                )
+                for inactive_ingredient_data in inactive_ingredients_data:
+                    try:
+                        ingredient = InactiveIngredient.objects.get(
+                            **inactive_ingredient_data
+                        )
+                        inactive_ingredients_list.append(ingredient)
+                    except Exception:
+                        ingredient = InactiveIngredient.objects.create(
+                            **inactive_ingredient_data
+                        )
+                        inactive_ingredients_list.append(ingredient)
+
             product = spl.products.create(**product_data)
+
+            product.inactive_ingredients.add(*inactive_ingredients_list)
+
             for package_data in packages_data:
                 product.packages.create(**package_data)
+
         return item
