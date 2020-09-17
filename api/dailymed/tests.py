f5961e3811656e3702db419624561bc7a6dece87
Caleb Dunn <finish.06@gmail.com>
2020-09-17 07:34:15 -0500

    add serializer tests

--- api/dailymed/tests.py
+++ api/dailymed/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
+from rest_framework import status
 from rest_framework.test import APIClient
 
 from dailymed.models import Set, Spl, Product, InactiveIngredient, Package
+
+from dailymed.serializers import SplSerializer, ProductSerializer
+from dailymed.serializers import PackageSerializer
 
 
 def sample_data(**params):
@@ -19,11 +24,13 @@ def sample_data(**params):
     return default
 
 
-class PublicApiTest(TestCase):
-    """Test public daily med API"""
+SPL_URL = reverse('spl-list')
+PRODUCT_URL = reverse('product-list')
+PACKAGE_URL = reverse('package-list')
 
-    def setUp(self):
-        self.client = APIClient()
+
+class DatabaseTest(TestCase):
+    """Test database creation and structure"""
 
     def test_create_set(self):
         """Test creation of set in db"""
@@ -97,3 +104,65 @@ class PublicApiTest(TestCase):
 
         expected_str = f"{package.code} -- {package.product}"
         self.assertEqual(str(package), expected_str)
+
+
+class PublicApiTest(TestCase):
+    """Test public daily med API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+        # Construct sample data in temp DB
+        data = sample_data()
+        set_obj = Set.objects.create(id=data['set_id'])
+        spl = Spl.objects.create(
+            id=data['id'],
+            set=set_obj,
+            labeler=data['labeler']
+        )
+        product = Product.objects.create(
+            code=data['products'][0]['code'],
+            name=data['products'][0]['name'],
+            spl=spl
+        )
+        package = Package.objects.create(
+            code=data['products'][0]['packages'][0]['code'],
+            product=product
+        )
+
+        self.set = set_obj
+        self.spl = spl
+        self.product = product
+        self.package = package
+
+        self.ingredient = InactiveIngredient.objects.create(
+            name="MICROCRYSTALLINE CELLULOSE",
+            unii="OP1R32D61U"
+        )
+
+    def test_retrieve_spl(self):
+        """Test retriving a spl"""
+        res = self.client.get(SPL_URL, format='json')
+
+        serializer = SplSerializer(self.spl)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(serializer.data, res.data)
+
+    def test_retrieve_product(self):
+        """Test retriving a product"""
+        res = self.client.get(PRODUCT_URL, format='json')
+
+        serializer = ProductSerializer(self.product)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(serializer.data, res.data)
+
+    def test_retrieve_package(self):
+        """Test retriving a package"""
+        res = self.client.get(PACKAGE_URL, format='json')
+
+        serializer = PackageSerializer(self.package)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(serializer.data, res.data)
