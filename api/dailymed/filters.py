5380311a34a443dbcde2cda98c2553873a644bfe
Joey LeGrand <jrlegrand@gmail.com>
2020-09-29 12:05:46 +0000

    Adding filters.py

---
+++ api/dailymed/filters.py
@@ -0,0 +1,16 @@
+from django_filters import rest_framework as filters
+from dailymed.models import Spl
+
+class SplFilter(filters.FilterSet):
+    set_id = filters.CharFilter(field_name='set', lookup_expr='iexact')
+    labeler = filters.CharFilter(field_name='labeler', lookup_expr='icontains')
+    package_ndc = filters.CharFilter(field_name='products__packages__code', lookup_expr='icontains')
+    product_ndc = filters.CharFilter(field_name='products__code', lookup_expr='icontains')
+    product_name = filters.CharFilter(field_name='products__name', lookup_expr='icontains')
+    inactive_ingredient_name = filters.CharFilter(field_name='products__inactive_ingredients__name', lookup_expr='icontains')
+    inactive_ingredient_unii = filters.CharFilter(field_name='products__inactive_ingredients__unii', lookup_expr='icontains')
+    schedule = filters.CharFilter(field_name='products__schedule', lookup_expr='iexact')
+
+    class Meta:
+        model = Spl
+        fields = []
