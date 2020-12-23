49fec0039b40fd8f68a9fd8ebb5ccfb3f9749921
Caleb Dunn <finish.06@gmail.com>
2020-12-23 07:06:03 -0600

    distinct get helper functions

--- scraper/scraper/utils.py
+++ scraper/scraper/utils.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 
 
-def get_filenames():
+def get_spls():
     cwd = Path(__file__)
-    partial_dir = cwd.parent.parent.parent.absolute() / 'data' / 'partial'
+    partial_dir = cwd.parent.parent.parent.absolute() / 'data' / 'spls'
     adjusted_filenames = [
         f'file://{path}' for path in list(partial_dir.iterdir())]
+    return adjusted_filenames
+
+
+def get_rxnorm():
+    cwd = Path(__file__)
+    rxnorm_dir = cwd.parent.parent.parent.absolute() / 'data' / 'rxnorm'
+    adjusted_filenames = [
+        f'{path}' for path in list(rxnorm_dir.iterdir())
+    ]
     return adjusted_filenames
